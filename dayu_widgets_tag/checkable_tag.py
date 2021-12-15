3e41a4d01e992d883d00b97a1ebf2ad50e817290
Yanru Mu <muyanru345@163.com>
2021-12-15 15:00:31 +0800

    feat: fix hight DPI problem

--- dayu_widgets_tag/checkable_tag.py
+++ dayu_widgets_tag/checkable_tag.py
@@ -11,6 +11,7 @@ MCheckableTag
 # Import third-party modules
 from dayu_widgets import dayu_theme
 from dayu_widgets.mixin import cursor_mixin
+from dayu_widgets.qt import get_scale_factor
 from dayu_widgets.theme import QssTemplate
 from qtpy.QtWidgets import QCheckBox
 
@@ -21,16 +22,17 @@ class MCheckableTag(QCheckBox):
 
     def __init__(self, text, parent=None):
         super(MCheckableTag, self).__init__(text, parent)
+        scale_x, x = get_scale_factor()
         self.setCheckable(True)
         style = QssTemplate(
             """
             MCheckableTag{
-                spacing: 8px;
-                padding-top: 4px;
-                padding-bottom: 4px;
-                padding-right: 8px;
+                spacing: @spaceing@unit;
+                padding-top: @padding@unit;
+                padding-bottom: @padding@unit;
+                padding-right: @spaceing@unit;
                 border-radius: @border_radius_base@unit;
-                font-size: @font_size_small@unit;
+                font-size: @font_size_base@font_unit;
             }
             MCheckableTag::indicator{
                 width: 0;
@@ -56,4 +58,18 @@ class MCheckableTag(QCheckBox):
             }
             """
         )
-        self.setStyleSheet(style.substitute(vars(dayu_theme)))
+        self.setStyleSheet(
+            style.substitute(
+                spaceing=8 * scale_x,
+                padding=4 * scale_x,
+                border_radius_base=dayu_theme.border_radius_base,
+                unit=dayu_theme.unit,
+                font_unit=dayu_theme.font_unit,
+                font_size_base=dayu_theme.font_size_base,
+                primary_5=dayu_theme.primary_5,
+                primary_7=dayu_theme.primary_7,
+                primary_color=dayu_theme.primary_color,
+                secondary_text_color=dayu_theme.secondary_text_color,
+                text_color_inverse=dayu_theme.text_color_inverse,
+            )
+        )
